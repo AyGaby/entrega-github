aa7decc55f773bbd8b68780469459532280be442
AyGaby <gbb888021@gmail.com>
2023-12-22 11:33:34 -0300

    cambios con asesorias

--- src/pages/Buscador.jsx
+++ src/pages/Buscador.jsx
@@ -1,46 +1,71 @@
 import { useState } from "react"; //useState de React. Gestiona el estado en componentes de función.
 import logo from "../assets/logui.png";
-
 import UserItem from "../components/UserItem";
-// import RepoItem from "../components/RepoItem";
+import RepoItem from "../components/RepoItem";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api/v1"; // https://api.github.com esto es lo viejo (conectado FRONT CON END?) http://localhost3000/api/v1
+const API_BASE_URL = "http://localhost:3000/api/v1";
+const GIT_API_URL = "https://api.github.com";
 
 const Buscador = () => {
   const navigate = useNavigate();
   const [query, setQuery] = useState("");
+  const [typeSearch, setTypeSearch] = useState("users", "repos");
 
   // Estados para almacenar resultados de usuarios y repositorio.
   const [userResults, setUserResults] = useState([]);
-  // const [repoResults, setRepoResults] = useState([]);
+  const [repoResults, setRepoResults] = useState([]);
+
   // Estado para almacenar el elemento seleccionado
   const [selectedItem, setSelectedItem] = useState(null);
 
-  // Función para realizar solicitudes a la API de GitHub
-  const fetchData = async (endpoint) => {
-    const response = await fetch(`${API_BASE_URL}${endpoint}`, {});
-    if (!response.ok) {
-      throw new Error(`GitHub API request failed: ${response.statusText}`);
+  const saveSearch = async (item) => {
+    try {
+      let databody = {};
+      const type = typeSearch === "users" ? "users" : "repos";
+      if (type === "users") {
+        databody = {
+          id: item.id,
+          name: item.login,
+          avatar_url: item.avatar_url,
+          repos_url: item.repos_url,
+          description: item.description,
+        };
+      } else {
+        databody = {
+          name: item.name,
+          score: item.score,
+          description: item.description,
+        };
+      }
+
+      const response = await axios.post(`${API_BASE_URL}/${type}`, databody);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
     }
-    const body = await response.json();
-    console.log(body);
-    return body;
   };
 
   // Función para manejar la búsqueda
   const handleSearch = async (e) => {
     e.preventDefault();
+
     try {
-      // const userData = await fetchData(`/users`);
-      // const repoData = await fetchData(`/search/repositories?q=${query}`);
+      console.log(typeSearch);
+      const response = await axios.get(
+        `${GIT_API_URL}/search/${typeSearch}?q=${query}`
+      );
 
-      const userData = await fetchData(`/users?q=${query}`);
+      if (response.data) {
+        setUserResults(response.data.items);
+      }
 
-      console.log(userData);
+      if (response.data && typeSearch === "repositories") {
+        setRepoResults(response.data.items);
+      }
 
-      setUserResults(userData.result.users || []);
-      // setRepoResults(repoData.items || []);
+      //setRepoResults(repoData.items || []);
     } catch (error) {
       console.error("Error fetching data: ", error);
     }
@@ -68,18 +93,29 @@ const Buscador = () => {
             </div>
           </div>
           <h4>Search</h4>
-          <form onSubmit={handleSearch}>
+          <form>
             <input
               type="text"
               value={query}
               onChange={handleInputChange}
-              placeholder="Enter username or repository name"
+              placeholder="Enter username or repository"
             />
-            <button type="submit">☆Good Luck☆</button>
+            <select
+              value={typeSearch}
+              onChange={(event) => {
+                setTypeSearch(event.target.value);
+              }}
+            >
+              <option value="users">Users</option>
+              <option value="repositories">Repositories</option>
+            </select>
           </form>
+          <button type="button" onClick={handleSearch}>
+            ☆Good Luck☆
+          </button>
         </div>
         <div className="results">
-          <h2>Users</h2>
+          <h2>{typeSearch}</h2>
           {userResults.map((item) => (
             <UserItem
               key={item.id}
@@ -87,22 +123,30 @@ const Buscador = () => {
               onClick={() => setSelectedItem(item)}
             />
           ))}
-          <h2>Repositories</h2>
-          {/* {repoResults.map((item) => (
-            <RepoItem
-              key={item.id}
-              item={item}
-              onClick={() => setSelectedItem(item)}
-            />
-          ))} */}
+          {repoResults !== null &&
+            repoResults.map((item) => (
+              <RepoItem
+                key={item.id}
+                item={item}
+                onClick={() => setSelectedItem(item)}
+              />
+            ))}
+          {!!selectedItem && (
+            <div className="details">
+              <h2>Details</h2>
+              <p>Full Name: {selectedItem?.login}</p>
+              <p>Score: {selectedItem?.score}</p>
+            </div>
+          )}
+          {!!selectedItem && (
+            <button
+              className="guardar"
+              onClick={() => saveSearch(selectedItem)}
+            >
+              Save ☆
+            </button>
+          )}
         </div>
-        {!!selectedItem && (
-          <div className="details">
-            <h2>Details</h2>
-            <p>Full Name: {selectedItem?.login}</p>
-            <p>Score: {selectedItem?.score}</p>
-          </div>
-        )}
       </div>
     </div>
   );
